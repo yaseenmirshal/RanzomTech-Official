a276cb6c8f7ec484577bec431480fafb0b94ec45
Yaseen Mirshal <yaseenmirshal5@gmail.com>
2026-05-06 07:28:18 +0530

    sitemap installed

---
+++ next-sitemap.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://ranzomtech.com', // 🔁 change if needed
+  generateRobotsTxt: true,
+  changefreq: 'weekly',
+  priority: 0.7,
+  sitemapSize: 7000,
+}
